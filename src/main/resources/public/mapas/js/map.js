7567b300927476d5a2b26b1dd6a3d41e5814b4d6
alejandro <20160338@ce.pucmm.edu.do>
2020-08-02 21:25:48 -0400

    Drop down menu in maps view

--- src/main/resources/public/mapas/js/map.js
+++ src/main/resources/public/mapas/js/map.js
@@ -103,7 +103,23 @@ function multipleDirections(map){
 
 
 }
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 document.getElementById("myBtn").addEventListener("click", multipleDirections);
         
